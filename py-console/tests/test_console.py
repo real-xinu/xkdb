ace1254b14c60d29c9eacfc293dd1952a7b8f737
Ammar Askar <ammar@ammaraskar.com>
2018-10-21 14:59:47 -0400

    Fix tests

--- py-console/tests/test_console.py
+++ py-console/tests/test_console.py
@@ -3,7 +3,7 @@ try:
     from unittest import mock
 except ImportError:
     import mock
-import console
+import xkdb
 
 
 class TestConsole(unittest.TestCase):
@@ -12,44 +12,44 @@ class TestConsole(unittest.TestCase):
         test_string = b"Hello\0World!\0This is a null terminated string"
 
         read_cursor = 0
-        string1, length = console.get_string(test_string[read_cursor:])
+        string1, length = xkdb.get_string(test_string[read_cursor:])
 
         self.assertEqual(string1, "Hello")
         self.assertEqual(length, 6)
 
         read_cursor += length
 
-        string2, length = console.get_string(test_string[read_cursor:])
+        string2, length = xkdb.get_string(test_string[read_cursor:])
         
         self.assertEqual(string2, "World!")
         self.assertEqual(length, 7)
 
         read_cursor += length
 
-        string3, length = console.get_string(test_string[read_cursor:])
+        string3, length = xkdb.get_string(test_string[read_cursor:])
 
         self.assertEqual(string3, "This is a null terminated string")
         self.assertEqual(length, 32)
 
 backends1 = [
-    console.Backend('xinu01', 'quark', None, None),
-    console.Backend('xinu02', 'galileo', 'anon', '21:30')
+    xkdb.Backend('xinu01', 'quark', None, None),
+    xkdb.Backend('xinu02', 'galileo', 'anon', '21:30')
 ]
 backends2 = [
-    console.Backend('xinu03', 'quark', None, None),
-    console.Backend('xinu04', 'quark', None, None)
+    xkdb.Backend('xinu03', 'quark', None, None),
+    xkdb.Backend('xinu04', 'quark', None, None)
 ]
 mock_servers = [
-    console.BackendServer('server1', 'server1.example.com', backends1),
-    console.BackendServer('server2', 'server2.example.com', backends2)
+    xkdb.BackendServer('server1', 'server1.example.com', backends1),
+    xkdb.BackendServer('server2', 'server2.example.com', backends2)
 ]
 
-@mock.patch('argparse._sys.argv', ['console.py', '--status'])
-@mock.patch('console.get_backend_servers')
+@mock.patch('argparse._sys.argv', ['xkdb.py', '--status'])
+@mock.patch('xkdb.get_backend_servers')
 def test_main_status(get_backend_servers, capsys):
     get_backend_servers.return_value = mock_servers
 
-    console.main()
+    xkdb.main()
 
     captured = capsys.readouterr()
 
@@ -61,23 +61,23 @@ def test_main_status(get_backend_servers, capsys):
     assert 'anon' in captured.out
     assert '21:30' in captured.out
 
-@mock.patch('argparse._sys.argv', ['console.py', 'xinu02'])
-@mock.patch('console.get_backend_servers')
+@mock.patch('argparse._sys.argv', ['xkdb.py', 'xinu02'])
+@mock.patch('xkdb.get_backend_servers')
 def test_get_specific_backend_in_use(get_backend_servers, capsys):
     get_backend_servers.return_value = mock_servers
 
-    console.main()
+    xkdb.main()
 
     get_backend_servers.assert_called_once()
     captured = capsys.readouterr()
     assert 'Backend xinu02 is in use by anon' in captured.out
 
-@mock.patch('argparse._sys.argv', ['console.py', 'nope'])
-@mock.patch('console.get_backend_servers')
+@mock.patch('argparse._sys.argv', ['xkdb.py', 'nope'])
+@mock.patch('xkdb.get_backend_servers')
 def test_get_specific_backend_non_existent(get_backend_servers, capsys):
     get_backend_servers.return_value = mock_servers
 
-    console.main()
+    xkdb.main()
 
     get_backend_servers.assert_called_once()
     captured = capsys.readouterr()
